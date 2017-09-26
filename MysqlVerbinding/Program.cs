b32e7b4539d8011ed42f8d78db424881f478c706
LiamB-immalle <liam.brangers@immalle.be>
2017-09-26 15:24:10 +0200

    Connection to mySQL-database

--- MysqlVerbinding/Program.cs
+++ MysqlVerbinding/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace MysqlVerbinding
 {
@@ -6,7 +7,19 @@ namespace MysqlVerbinding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString =
+            "Server=192.168.56.101;Port=3306;Database=Concerten;Uid=imma;Pwd=immapwd;";
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT count(*) FROM Artiesten";
+
+            conn.Open();
+
+            int aantalArtiesten = Convert.ToInt32(cmd.ExecuteScalar());
+
+            Console.WriteLine("Aantal artiesten : {0}", aantalArtiesten);
         }
     }
 }
